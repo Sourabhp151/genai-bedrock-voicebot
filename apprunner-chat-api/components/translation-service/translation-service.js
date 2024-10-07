b9fbe4ae5349614195b6ddbe6b8656629e3051ff
Sourabhp151 <125236358+Sourabhp151@users.noreply.github.com>
2024-10-07 13:13:49 +0530

    Update translation-service.js

--- apprunner-chat-api/components/translation-service/translation-service.js
+++ apprunner-chat-api/components/translation-service/translation-service.js
@@ -6,6 +6,37 @@ async function translateText(text, sourceLanguageCode, targetLanguageCode) {
 
   if (
     (sourceLanguageCode === "hi" && targetLanguageCode === "en") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "hi") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "te") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "ta") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "kn") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "hi") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "pa") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "gu") 
+  ) {
+    return await awsTranslate(text, sourceLanguageCode, targetLanguageCode);
+  } else {
+    return await bhashiniTranslation(
+      text,
+      sourceLanguageCode,
+      targetLanguageCode
+    );
+  }
+}
+
+module.exports = { translateText };
+
+
+
+
+/*const { awsTranslate } = require("./aws-translate");
+const { bhashiniTranslation } = require("./bhashini-translation");
+
+async function translateText(text, sourceLanguageCode, targetLanguageCode) {
+  if (sourceLanguageCode === targetLanguageCode) return text;
+
+  if (
+    (sourceLanguageCode === "hi" && targetLanguageCode === "en") ||
     (sourceLanguageCode === "en" && targetLanguageCode === "hi")
   ) {
     return await awsTranslate(text, sourceLanguageCode, targetLanguageCode);
@@ -19,3 +50,4 @@ async function translateText(text, sourceLanguageCode, targetLanguageCode) {
 }
 
 module.exports = { translateText };
+*/
